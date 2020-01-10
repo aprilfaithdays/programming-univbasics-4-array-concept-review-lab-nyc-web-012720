b4d817fc754e8f05ffa2c2559d141dcc846eb818
April Escobar <aprilfaith.e@gmail.com>
2020-01-10 02:44:01 +0000

    Done.

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,13 +1,53 @@
 def find_element_index(array, value_to_find)
-    array.index(value_to_find)
+  
+  array.length.times do |count|
+   
+    if array[count] == value_to_find
+     
+      return count
+    
+    end
+  
+  end
+  
+  nil
+
 end
 
 
 def find_max_value(array)
-  array.max
+  
+  max = array[0]
+  
+  array.length.times do |index|
+   
+    if array[index] > max
+  
+      max = array[index]
+    
+    end
+  
+  end
+  
+  max
+  
 end
 
 
 def find_min_value(array)
-  array.min
+  
+   min = array[0]
+  
+  array.length.times do |index|
+   
+    if array[index] < min
+  
+      min = array[index]
+    
+    end
+  
+  end
+  
+  min
+  
 end
