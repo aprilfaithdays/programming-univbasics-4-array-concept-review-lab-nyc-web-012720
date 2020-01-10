08c63620277be9023401db7d0aa7bdba80172992
April Escobar <aprilfaith.e@gmail.com>
2020-01-10 02:44:32 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -15,6 +15,7 @@ def find_element_index(array, value_to_find)
 end
 
 
+
 def find_max_value(array)
   
   max = array[0]
@@ -32,6 +33,7 @@ def find_max_value(array)
   max
   
 end
+
 
 
 def find_min_value(array)
